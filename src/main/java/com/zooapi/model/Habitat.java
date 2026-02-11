84d2aacfa976d8853d3e497835efdaaa47114363
Francisco <franknajeraps4@gmail.com>
2026-02-10 17:43:50 -0800

    Initial commit

---
+++ src/main/java/com/zooapi/model/Habitat.java
@@ -0,0 +1,26 @@
+package com.zooapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Setter
+@Getter
+
+public class Habitat {
+    public Habitat (String nombre, int capacidad, int habitatId){
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.animales= new ArrayList<>();
+        this.habitatId = habitatId;
+    }
+    public String nombre;
+    public int capacidad;
+    public List<Animal> animales;
+    
+    private int habitatId;
+
+    public void setHabitatId(int habitatId) {
+    }
+}
